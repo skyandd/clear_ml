e47238e485428425619846d4bb7477db2faaec3c
anton <addudkin@dbrain.io>
2023-01-18 21:09:53 +0800

    Change clear ml

--- pytorch_mnist.py
+++ pytorch_mnist.py
@@ -81,12 +81,12 @@ def main():
     task = Task.init(project_name='Demo', task_name='Remote_execution PyTorch MNIST train')
     # Training settings
     parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
-    parser.add_argument('--batch-size', type=int, default=32, metavar='N',
+    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
                         help='input batch size for training (default: 64)')
     parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
                         help='input batch size for testing (default: 1000)')
-    parser.add_argument('--epochs', type=int, default=2, metavar='N',
-                        help='number of epochs to train (default: 2)')
+    parser.add_argument('--epochs', type=int, default=3, metavar='N',
+                        help='number of epochs to train (default: 3)')
     parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
                         help='learning rate (default: 0.01)')
     parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
--- test_pipe.py
+++ test_pipe.py
@@ -1,18 +1,18 @@
-from clearml import PipelineController
+from clearml.automation.controller import PipelineDecorator
+from clearml import TaskTypes
 
 
-# We will use the following function an independent pipeline component step
+# Make the following function an independent pipeline component step
 # notice all package imports inside the function will be automatically logged as
 # required packages for the pipeline execution step
-def step_one(pickle_data_url):
+@PipelineDecorator.component(return_values=['data_frame'], cache=True, task_type=TaskTypes.data_processing)
+def step_one(pickle_data_url: str, extra: int = 43):
+    print('step_one')
     # make sure we have scikit-learn for this step, we need it to use to unpickle the object
     import sklearn  # noqa
     import pickle
     import pandas as pd
     from clearml import StorageManager
-    pickle_data_url = \
-        pickle_data_url or \
-        'https://github.com/allegroai/events/raw/master/odsc20-east/generic/iris_dataset.pkl'
     local_iris_pkl = StorageManager.get_local_copy(remote_url=pickle_data_url)
     with open(local_iris_pkl, 'rb') as f:
         iris = pickle.load(f)
@@ -22,83 +22,99 @@ def step_one(pickle_data_url):
     return data_frame
 
 
-# We will use the following function an independent pipeline component step
+# Make the following function an independent pipeline component step
 # notice all package imports inside the function will be automatically logged as
-# required packages for the pipeline execution step
+# required packages for the pipeline execution step.
+# Specifying `return_values` makes sure the function step can return an object to the pipeline logic
+# In this case, the returned tuple will be stored as an artifact named "X_train, X_test, y_train, y_test"
+@PipelineDecorator.component(
+    return_values=['X_train, X_test, y_train, y_test'], cache=True, task_type=TaskTypes.data_processing
+)
 def step_two(data_frame, test_size=0.2, random_state=42):
+    print('step_two')
     # make sure we have pandas for this step, we need it to use the data_frame
     import pandas as pd  # noqa
     from sklearn.model_selection import train_test_split
     y = data_frame['target']
     X = data_frame[(c for c in data_frame.columns if c != 'target')]
     X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=test_size, random_state=random_state)
+        X, y, test_size=test_size, random_state=random_state
+    )
 
     return X_train, X_test, y_train, y_test
 
 
-# We will use the following function an independent pipeline component step
+# Make the following function an independent pipeline component step
 # notice all package imports inside the function will be automatically logged as
 # required packages for the pipeline execution step
-def step_three(data):
+# Specifying `return_values` makes sure the function step can return an object to the pipeline logic
+# In this case, the returned object will be stored as an artifact named "model"
+@PipelineDecorator.component(return_values=['model'], cache=True, task_type=TaskTypes.training)
+def step_three(X_train, y_train):
+    print('step_three')
     # make sure we have pandas for this step, we need it to use the data_frame
     import pandas as pd  # noqa
     from sklearn.linear_model import LogisticRegression
-    X_train, X_test, y_train, y_test = data
     model = LogisticRegression(solver='liblinear', multi_class='auto')
     model.fit(X_train, y_train)
     return model
 
+# Make the following function an independent pipeline component step
+# notice all package imports inside the function will be automatically logged as
+# required packages for the pipeline execution step
+# Specifying `return_values` makes sure the function step can return an object to the pipeline logic
+# In this case, the returned object will be stored as an artifact named "accuracy"
+@PipelineDecorator.component(return_values=['accuracy'], cache=True, task_type=TaskTypes.qc)
+def step_four(model, X_data, Y_data):
+    from sklearn.linear_model import LogisticRegression  # noqa
+    from sklearn.metrics import accuracy_score
+    Y_pred = model.predict(X_data)
+    return accuracy_score(Y_data, Y_pred, normalize=True)
+
+# The actual pipeline execution context
+# notice that all pipeline component function calls are actually executed remotely
+# Only when a return value is used, the pipeline logic will wait for the component execution to complete
+@PipelineDecorator.pipeline(name='custom pipeline logic', project='examples', version='0.0.5')
+def executing_pipeline(pickle_url, mock_parameter='mock'):
+    print('pipeline args:', pickle_url, mock_parameter)
+
+    # Use the pipeline argument to start the pipeline and pass it ot the first step
+    print('launch step one')
+    data_frame = step_one(pickle_url)
+
+    # Use the returned data from the first step (`step_one`), and pass it to the next step (`step_two`)
+    # Notice! unless we actually access the `data_frame` object,
+    # the pipeline logic does not actually load the artifact itself.
+    # When actually passing the `data_frame` object into a new step,
+    # It waits for the creating step/function (`step_one`) to complete the execution
+    print('launch step two')
+    X_train, X_test, y_train, y_test = step_two(data_frame)
+
+    print('launch step three')
+    model = step_three(X_train, y_train)
+
+    # Notice since we are "printing" the `model` object,
+    # we actually deserialize the object from the third step, and thus wait for the third step to complete.
+    print('returned model: {}'.format(model))
+
+    print('launch step four')
+    accuracy = 100 * step_four(model, X_data=X_test, Y_data=y_test)
+
+    # Notice since we are "printing" the `accuracy` object,
+    # we actually deserialize the object from the fourth step, and thus wait for the fourth step to complete.
+    print(f"Accuracy={accuracy}%")
+
 
 if __name__ == '__main__':
+    # set the pipeline steps default execution queue (per specific step we can override it with the decorator)
+    # PipelineDecorator.set_default_execution_queue('default')
+    # Run the pipeline steps as subprocesses on the current machine, great for local executions
+    # (for easy development / debugging, use `PipelineDecorator.debug_pipeline()` to execute steps as regular functions)
+    PipelineDecorator.run_locally()
 
-    # create the pipeline controller
-    pipe = PipelineController(
-        project='examples',
-        name='Pipeline demo',
-        version='1.1',
-        add_pipeline_tags=False,
+    # Start the pipeline execution logic.
+    executing_pipeline(
+        pickle_url='https://github.com/allegroai/events/raw/master/odsc20-east/generic/iris_dataset.pkl',
     )
 
-    # set the default execution queue to be used (per step we can override the execution)
-    pipe.set_default_execution_queue('default')
-
-    # add pipeline components
-    pipe.add_parameter(
-        name='url',
-        description='url to pickle file',
-        default='https://github.com/allegroai/events/raw/master/odsc20-east/generic/iris_dataset.pkl'
-    )
-    pipe.add_function_step(
-        name='step_one',
-        function=step_one,
-        function_kwargs=dict(pickle_data_url='${pipeline.url}'),
-        function_return=['data_frame'],
-        cache_executed_step=True,
-    )
-    pipe.add_function_step(
-        name='step_two',
-        # parents=['step_one'],  # the pipeline will automatically detect the dependencies based on the kwargs inputs
-        function=step_two,
-        function_kwargs=dict(data_frame='${step_one.data_frame}'),
-        function_return=['processed_data'],
-        cache_executed_step=True,
-    )
-
-    pipe.add_function_step(
-        name='step_three',
-        # parents=['step_two'],  # the pipeline will automatically detect the dependencies based on the kwargs inputs
-        function=step_three,
-        function_kwargs=dict(data='${step_two.processed_data}'),
-        function_return=['model'],
-        cache_executed_step=True,
-    )
-
-    # For debugging purpos run on the pipeline on current machine
-    # Use run_pipeline_steps_locally=True to further execute the pipeline component Tasks as subprocesses.
-    # pipe.start_locally(run_pipeline_steps_locally=False)
-
-    # Start the pipeline on the services queue (remote machine, default on the clearml-server)
-    pipe.start(queue='default')
-
-    print('pipeline completed')
+    print('process completed')
